4d2656d25edad208b7d2d51bd174891f059a66e3
liuhaoliangdashabi <2970929417@qq.com>
2026-09-12 20:49:01 +0800

    路由选择没做完

---
+++ ai-vue/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ElMessage } from 'element-plus'
+import BackendLayout from './components/BackendLayout.vue'
+
+const handleClick = () => {
+  ElMessage.success('Element Plus 引入成功')
+}
+</script>
+
+<template>
+  <router-view/>
+</template>
---
+++ ai-vue/src/components/BackendLayout.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="backend-layout">
+    <el-container class="main-container">
+      <SideBar/>
+      <el-container>
+        <Navbar/>
+        <router-view/>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+import SideBar from './SideBar.vue'
+import Navbar from './Navbar.vue'
+</script>
+
+<style lang="scss" scoped>
+.backend-layout{
+    height:100vh;
+    .main-container{
+        height:100%;
+    }
+}
+
+</style>
